25d94a86792612f5f4a6eddd0c331810f60b2a2c
MaeshiroJunji <junjimaeshiro0613@gmail.com>
2021-11-19 20:55:34 +0900

    apiの編集

--- maeshiro/asset/js/api.js
+++ maeshiro/asset/js/api.js
@@ -1,32 +1,38 @@
-$(function(){
+$(function () {
 
-  $("#start").on("click",function(){
-      let input = $("#input").val()
-      console.log(input);
+  // $('.btn').on('click', function () {
+      // https://www.sysbird.jp/webapi/?apikey=guest&keyword=%E3%82%AB%E3%83%AC%E3%83%BC&max=10&order=r
 
       let data = {
-        keyword:"チョコ"
+         keyword:'チョコ',
+         apikey:'guest',
+         format:'jsonp',
+         order:'r'
       }
- 
-  $.ajax({
-  url: "https://sysbird.jp/toriko/api/",
-  type: "get",　　 //post or get
-  cache: false,        //cacheを使うか使わないかを設定
-  dataType:"jsonp",     //data type script・xmlDocument・jsonなど
-  data:data,   
-  })  
 
-.done(function(response) { 
-   //通信成功時の処理
-   //成功したとき実行したいスクリプトを記載
-   console.log(response);
+      $.ajax({
+          url: 'https://sysbird.jp/toriko/api/', //アクセスするURL
+          type: 'get',　　 //post or get
+          cache: false,        //cacheを使うか使わないかを設定
+          dataType: 'jsonp',     //data type script・xmlDocument・jsonなど
+          data: data,           //アクセスするときに必要なデータを記載   連想配列でデータを渡す   
+      })
+          .done(function (response) {
+              //通信成功時の処理
+              console.log(response["item"]);
+              var oyatsu =response["item"];
+              console.log(oyatsu[0]);
+              console.log(oyatsu[0]["image"]);
+              //成功したとき実行したいスクリプトを記載
+          })
+          .fail(function (xhr) {
+              //通信失敗時の処理
+              //失敗したときに実行したいスクリプトを記載
+          })
+          .always(function (xhr, msg) {
+              //通信完了時の処理
+              //結果に関わらず実行したいスクリプトを記載
+          });
+  // })
+})
 
-})
-.fail(function(xhr) {  
-  //通信失敗時の処理
-   //失敗したときに実行したいスクリプトを記載
-   console.log(xhr);
-
-})
-})
-})
