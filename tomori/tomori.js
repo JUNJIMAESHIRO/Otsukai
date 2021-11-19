e6c3f8563c97299a1eac86bc98b14f4e9f3c3aba
LAPTOP-GSD0DSCE\Sakura <Daima69.st@gmail.com>
2021-11-19 20:52:40 +0900

    チェックボタンを押すと、チェックボタンの言葉（チョコなど）をコンソールログで出力し それに関連するお菓子をランダムでとってくるようにｊｓを書きました

--- tomori/tomori.js
+++ tomori/tomori.js
@@ -1,40 +1,40 @@
-$(function(){
+$(function () {
 
-    $(".btn").on("click",function(){
-        let input = $("#input").val()
-        console.log(input);
+  $("#start").on("click", function () {
+    let a = $('input:radio[name="Oyatsu"]:checked').val();
+    // $('.m_middle').text(a);
+    console.log(a);
+      let data = {
+        keyword:a,
+        apikey:'guest',
+        format:'jsonp',
+        order:'r'
+     }
 
-        let data = {
-          keyword:input,
-          apikey:'guest',
-          format:'jsonp',
-          order:'r'
-       }
-   
     $.ajax({
-    url: "https://sysbird.jp/toriko/api/",
-    type: "get",　　 //post or get
-    cache: false,        //cacheを使うか使わないかを設定
-    dataType:"jsonp",     //data type script・xmlDocument・jsonなど
-    data: data,   
-    })  
-  
-  .done(function(response) { 
-     //通信成功時の処理
-     //成功したとき実行したいスクリプトを記載
-     console.log(response);
-  })
-
-  .fail(function(xhr) {  
-    //通信失敗時の処理
-     //失敗したときに実行したいスクリプトを記載
-     console.log(xhr);
+      url: "https://sysbird.jp/toriko/api/",
+      type: "get",　　 //post or get
+      cache: false,        //cacheを使うか使わないかを設定
+      dataType: "jsonp",     //data type script・xmlDocument・jsonなど
+      data: data,
     })
 
-    .always(function (xhr, msg) {
-      //通信完了時の処理
-      //結果に関わらず実行したいスクリプトを記載
-  });
-  
- })
+      .done(function (response) {
+        //通信成功時の処理
+        //成功したとき実行したいスクリプトを記載
+        console.log(response);
+      })
+
+      .fail(function (xhr) {
+        //通信失敗時の処理
+        //失敗したときに実行したいスクリプトを記載
+        console.log(xhr);
+      })
+
+      .always(function (xhr, msg) {
+        //通信完了時の処理
+        //結果に関わらず実行したいスクリプトを記載
+      });
+
+  })
 })
